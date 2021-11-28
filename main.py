9c1447ca2e8e6b1a592219c71775af4acfc8d4f9
daniwalter001 <daniwalter001@gmail.com>
2021-11-28 21:11:36 +0000

    Menu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import bs4
 import cfscrape
+import os
+import time
 
 
 headers_g = {
@@ -7,9 +9,12 @@ headers_g = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0"
 }
 
-# vdisplay = Display(visible=0, size=(1024, 768))
-# vdisplay.start() #to not display any browser
+
 cf = cfscrape.create_scraper()
+
+
+def clear():
+    os.system('cls||clear')
 
 
 def getInfos(node):
@@ -23,7 +28,11 @@ def getInfos(node):
     return infos
 
 
-def getMore():
+def getMorePage(start=1, end=1):
+
+    list = []
+    url = "https://mangas-origines.fr/wp-admin/admin-ajax.php"
+
     headers_c = {
         "Host": "mangas-origines.fr",
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
@@ -41,7 +50,7 @@ def getMore():
 
     data = {
         "action": "madara_load_more",
-        "page": "3",
+        # "page": str(start),
         "template":	"madara-core/content/content-archive",
         "vars[paged]": "1",
         "vars[orderby]": "meta_value_num",
@@ -64,23 +73,25 @@ def getMore():
         'vars[meta_query][relation]': "AND",
         'vars[manga_archives_item_layout]': "default"
     }
-    req = cf.post(
-        "https://mangas-origines.fr/wp-admin/admin-ajax.php", data, headers=headers_c)
 
-    return req
+    if start >= 0 and end and start < end:
+        for i in range(start, end+1):
+            data["page"] = str(i)
+            req = cf.post(url, data, headers=headers_c)
+            print("Fetching page {}...\n ".format(str(i)))
+            time.sleep(2)
+            list.extend(getData(req))
+    else:
+        data["page"] = str(start)
+        req = cf.post(url, data, headers=headers_c)
+        print("Fetching page {}...".format(str(start)))
+        list.extend(getData(req))
+
+    return list
 
 
-def getCatalogue():
-
-    link = "https://mangas-origines.fr/catalogue/"
-
-    # btn btn-default load-ajax
-
-    catalogue_request = cf.get(link, headers=headers_g)
-    # print(catalogue_request.headers)
-    catalogue_request = getMore()
-
-    soup = bs4.BeautifulSoup(catalogue_request.content, 'lxml')
+def getData(req):
+    soup = bs4.BeautifulSoup(req.content, 'lxml')
     req_result = soup.select(
         "div.col-12.col-md-4.badge-pos-1 > div.page-item-detail.manga")
 
@@ -88,10 +99,161 @@ def getCatalogue():
     for node in req_result:
         list.append(getInfos(node))
 
-    for i in list:
-        print(i)
-    print("nbre d'elements:", len(list))
+    return list
+
+
+def getCatalogue():
+    link = "https://mangas-origines.fr/catalogue/"
+
+    catalogue_request = cf.get(link, headers=headers_g)
+    print("Getting Catalogue Default Content...")
+    return getData(catalogue_request)
+
+# ---------------------------------LET S GO------------------------
+
+
+menu_options = {
+    1: 'Get default Catalogue',
+    2: 'Get Catalogue on a certain page',
+    3: 'Get Catalogue on a certain range',
+    4: 'Exit',
+}
+
+
+def print_menu():
+    for key in menu_options.keys():
+        print(key, '--', menu_options[key])
+
+
+def option1():
+    list = []
+    list.extend(getCatalogue())
+    for i, el in enumerate(list):
+        print("{}- {}".format(i+1, el['name']))
+    print('------')
+    while(True):
+        option1 = ''
+        try:
+            option1 = int(input('Enter your choice: '))
+            if option1 == 0:
+                clear()
+                break
+            elif option1 in range(1, len(list)+1):
+                print(list[option1-1])
+        except:
+            print('Wrong input. Please enter a number ...')
+
+
+def option2():
+
+    print("""
+    * Enter the page's catalogue you want to be displayed.
+    * If not entered, the first page catalogue will be
+      displayed
+
+    """)
+
+    start = 1
+
+    while True:
+        list = []
+        try:
+            print("Enter 0 to exit")
+            start = int(input("Which page? : "))
+            if start == 0:
+                break
+
+            list.extend(getCatalogue())
+            list.extend(getMorePage(start))
+
+            for i, el in enumerate(list):
+                print("{}- {}".format(i+1, el['name']))
+            print('------')
+            while(True):
+                option1 = ''
+                try:
+                    option1 = int(input('Enter your choice: '))
+                    if option1 == 0:
+                        clear()
+                        break
+                    elif option1 in range(1, len(list)+1):
+                        print(list[option1-1])
+                    else:
+                        print("You have {} elements displayed. You must know the range".format(
+                            str(len(list))))
+                except:
+                    print('Wrong input. Please enter a number ...')
+        except ValueError:
+            print('Start and End must be integers\n')
+            clear()
+            continue
+
+
+def option3():
+    print("""
+    * <<start>> and <<end>> is extrems of the range.
+    *If <<end>> is not entered, the (start)th page catalogue will be 
+    displayed
+        
+    """)
+
+    while True:
+        list = []
+        try:
+            print("Enter 0 to exit")
+            start = int(input("Start: "))
+            if start == 0:
+                break
+            end = int(input("End: "))
+
+            list.extend(getCatalogue())
+            list.extend(getMorePage(start, end))
+
+            for i, el in enumerate(list):
+                print("{}- {}".format(i+1, el['name']))
+            print('------')
+            while(True):
+                option1 = ''
+                try:
+                    option1 = int(input('Enter your choice: '))
+                    if option1 == 0:
+                        clear()
+                        break
+                    elif option1 in range(1, len(list)+1):
+                        print(list[option1-1])
+                    else:
+                        print("You have {} elements displayed. You must know the range".format(
+                            str(len(list))))
+                except:
+                    print('Wrong input. Please enter a number ...')
+        except ValueError:
+            print('Start and End must be integers\n')
+            clear()
+            continue
 
 
 if __name__ == "__main__":
-    getCatalogue()
+    try:
+        clear()
+
+        while(True):
+            print_menu()
+            option = ''
+            try:
+                option = int(input('Enter your choice: '))
+            except:
+                print('Wrong input. Please enter a number ...')
+            if option == 1:
+                option1()
+            elif option == 2:
+                option2()
+            elif option == 3:
+                option3()
+            elif option == 4:
+                print('Bye...')
+                exit()
+            else:
+                print('Invalid option. Please enter a number between 1 and 4.')
+
+    except KeyboardInterrupt:
+        print('\nProgram exited')
